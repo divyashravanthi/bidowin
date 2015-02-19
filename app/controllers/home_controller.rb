6b34941f26d920eb778e76548e20e59296e0f87e
Ankit Samarthya <ankitsamar@outlook.com>
2015-02-19 22:34:47 +0530

    trending done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
   	@search = Secret.search(params[:q])
-  	@secrets = @search.result
+  	if params[:hash].present?
+  		@hashtag = SimpleHashtag::Hashtag.find_by_name(params[:hash])
+    	@secrets = @hashtag.hashtaggables if @hashtag
+  	else
+  		@secrets = @search.result
+  	end
   end
 end
